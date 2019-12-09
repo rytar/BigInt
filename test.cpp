6b86df59ee39390bc02c37b380509b33ad73ad73
Rytar <53173674+rytar@users.noreply.github.com>
2019-12-09 15:18:38 +0900

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -18,6 +18,16 @@ void value_test(T n, T k) {
     std::cout << n << " >> 2 = " << (n >> 2) << std::endl;
     std::cout << k << " << 3 = " << (k << 3) << std::endl;
     std::cout << (-n).abs() << " " << (-k).abs() << std::endl;
+    std::cout << "char: " << (char)n << " " << (char)k << std::endl;
+    std::cout << "unsigned char: " << (unsigned char)n << " " << (unsigned char)k << std::endl;
+    std::cout << "short: " << (short)n << " " << (short)k << std::endl;
+    std::cout << "unsigned short: " << (unsigned short)n << " " << (unsigned short)k << std::endl;
+    std::cout << "int: " << (int)n << " " << (int)k << std::endl;
+    std::cout << "unsigned int: " << (unsigned int)n << " " << (unsigned int)k << std::endl;
+    std::cout << "long: " << (long)n << " " << (long)k << std::endl;
+    std::cout << "unsigned long: " << (unsigned long)n << " " << (unsigned long)k << std::endl;
+    std::cout << "long long: " << (long long)n << " " << (long long)k << std::endl;
+    std::cout << "unsigned long long: " << (unsigned long long)n << " " << (unsigned long long)k << std::endl;
 }
 
 template<typename T>
