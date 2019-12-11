da8b7553d8db16e6309dbcab37952d80882b2d4e
Rytar <53173674+rytar@users.noreply.github.com>
2019-12-11 16:54:09 +0900

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -3,9 +3,10 @@
 #include <climits>
 #include "BigInt.h"
 
-template<typename T>
-void value_test(T n, T k) {
+void value_test(BigInt n, BigInt k) {
     std::cout << BigInt("1234567890") << std::endl;
+    std::cout << BigInt(std::string("1234567890")) << std::endl;
+    std::cout << BigInt(1234567890) << std::endl;
     std::cout << n << " + " << k << " = " << (n + k) << std::endl;
     std::cout << n << " - " << k << " = " << (n - k) << std::endl;
     std::cout << n << " * " << k << " = " << (n * k) << std::endl;
@@ -29,25 +30,41 @@ void value_test(T n, T k) {
     std::cout << "unsigned long: " << (unsigned long)n << " " << (unsigned long)k << std::endl;
     std::cout << "long long: " << (long long)n << " " << (long long)k << std::endl;
     std::cout << "unsigned long long: " << (unsigned long long)n << " " << (unsigned long long)k << std::endl;
-    for(auto i : n.to_binary()) std::cout << i;
-    std::cout << std::endl << binary_to_i(n.to_binary()) << std::endl;
+    // std::cout << "float: " << (float)n << " " << (float)k << std::endl;
+    // std::cout << "double: " << (double)n << " " << (double)k << std::endl;
 }
 
 template<typename T>
-void time_test(T n, T k) {
+void time_test(T begin, T count) {
     auto start = std::chrono::system_clock::now();
-    for(T i = n; i < k; ++i)
+    for(T i = begin; i < begin + count; ++i)
         std::cout << i << " * " << i << " = " << (i * i) << std::endl;
     auto end = std::chrono::system_clock::now();
     auto ms = std::chrono::duration_cast<std::chrono::milliseconds>
               (end - start).count();
-    std::cout << "time(ave): " << (ms / (k - n)) << "[ms]" << std::endl;
+    std::cout << "time: " << ms << "[ms]" << std::endl;
+    std::cout << "ave: " << (ms / count) << "[ms]" << std::endl;
 }
+
+template<typename T>
+void test() {
+    T n = 4;
+    auto start = std::chrono::system_clock::now();
+    for(size_t i = 0; i < 4; i++)
+        std::cout << n << " * " << n << " = " << (n *= n) << std::endl;
+    auto end = std::chrono::system_clock::now();
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>
+              (end - start).count();
+    std::cout << "time: " << ms << "[ms]" << std::endl;
+}
+
 
 int main() {
     BigInt n, k;
     std::cout << "input: ";
     std::cin >> n >> k;
-    time_test(n, k);
+    // time_test(n, k);
     value_test(n, k);
+
+    // test<int>();
 }
